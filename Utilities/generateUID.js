fb24db2b6be2131fe436171e6592480292eaa88c
Ahmed Gaber <ahmed.gaber6@vodafone.com.eg>
2024-09-14 21:40:01 +0300

    vfkjdn

--- Utilities/generateUID.js
+++ Utilities/generateUID.js
@@ -1,10 +1,24 @@
+const {v6: uuidv6} = require('uuid')
+
 function generateUUID() {
-  const chars =
-    "0123456789";
-  return "xxxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-    const uid = Math.floor(Math.random() * chars.length);
-    return Number(chars[uid]);
-  });
+  // const chars =
+  //   "0123456789";
+  // return "xxxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+  //   const uid = Math.floor(Math.random() * chars.length);
+  //   return Number(chars[uid]);
+  // });
+
+  const uuidToNumeric = (uuid) => {
+    const hexStr = uuid.replace(/-/g, "")
+
+    const bigInt = BigInt("0x" + hexStr)
+
+    return bigInt.toString()
+  }
+
+  const uuid = uuidv6();
+  console.log(uuid)
+  return uuidToNumeric(uuid)
 }
 
 module.exports = { generateUUID };
